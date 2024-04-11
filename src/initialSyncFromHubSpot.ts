d83c0f2fde1afaba29eafe3a5230bba200a5b4f6
zwolfson <zwolfson@hubspot.com>
2024-04-11 15:24:19 -0400

    Rename file to be more consistent and add additional type annotations

--- src/initialSyncFromHubSpot.ts
+++ src/initialSyncFromHubSpot.ts
@@ -1,62 +1,58 @@
 import 'dotenv/config';
 
-import { Contacts, PrismaClient, Prisma } from '@prisma/client';
-import { Client as HubSpotClient } from '@hubspot/api-client';
+import { Contacts, Prisma } from '@prisma/client';
+
 import { SimplePublicObject } from '@hubspot/api-client/lib/codegen/crm/contacts';
 import { getAccessToken } from './auth';
 import { getCustomerId } from './utils';
+import { hubspotClient, prisma } from './clients';
 
 // Use verbose (but slower) create or update functionality
 const useVerboseCreateOrUpdate: boolean = false;
 
 // HubSpot client rate limit settings
-const DEFAULT_LIMITER_OPTIONS = {
-  minTime: 1000 / 9,
-  maxConcurrent: 6,
-  id: 'hubspot-client-limiter'
-};
 
 // HubSpot Client arguments
-// Unused values must be undefined to avoid HubSpot client errors 
+// Unused values must be undefined to avoid HubSpot client errors
 const pageLimit: number = 100;
 let after: string;
-const propertiesToGet: string[]= ['firstname', 'lastname', 'email'];
+const propertiesToGet: string[] = ['firstname', 'lastname', 'email'];
 let propertiesToGetWithHistory: string[];
 let associationsToGet: string[];
 const getArchived: boolean = false;
 
 // Types for handling create/update results
 type CreateUpdateUpsertResult = {
-  recordDetails: Contacts,
-  updateResult: string,
+  recordDetails: Contacts;
+  updateResult: string;
 };
 
 type JobRunResults = {
   upsert: {
-    count: number,
-    records: CreateUpdateUpsertResult[]
-  },
+    count: number;
+    records: CreateUpdateUpsertResult[];
+  };
   created: {
-    count: number,
-    records: CreateUpdateUpsertResult[]
-  },
+    count: number;
+    records: CreateUpdateUpsertResult[];
+  };
   failed: {
-    count: number,
-    records: CreateUpdateUpsertResult[]
-  },
+    count: number;
+    records: CreateUpdateUpsertResult[];
+  };
   hsID_updated: {
-    count: number,
-    records: CreateUpdateUpsertResult[]
-  },
+    count: number;
+    records: CreateUpdateUpsertResult[];
+  };
   errors: {
-    count: number,
-    records: CreateUpdateUpsertResult[]
-  }
+    count: number;
+    records: CreateUpdateUpsertResult[];
+  };
 };
 
-// Avoid overloading Prisma connections
-const prisma = new PrismaClient();
-
+interface ContactsWithEmail extends SimplePublicObject {
+  properties: { email: string };
+}
 // Update function 1 - use upsert to create or update records
 // Faster but less verbose tracking of created vs. updated
 const upsertContact = async (contactData: SimplePublicObject) => {
@@ -66,10 +62,11 @@ const upsertContact = async (contactData: SimplePublicObject) => {
     // Create the contact if no matching email
     // On matching email, update the HS ID but nothing else
     upsertRecord = await prisma.contacts.upsert({
-      where:{
+      where: {
         email: contactData.properties.email
       },
-      update: { // add the hs ID but don't update anything else
+      update: {
+        // add the hs ID but don't update anything else
         hs_object_id: contactData.id
       },
       create: {
@@ -79,7 +76,7 @@ const upsertContact = async (contactData: SimplePublicObject) => {
         hs_object_id: contactData.id
       }
     });
-    upsertResult = "upsert";
+    upsertResult = 'upsert';
   } else {
     // no email, create without email
     upsertRecord = await prisma.contacts.create({
@@ -89,15 +86,15 @@ const upsertContact = async (contactData: SimplePublicObject) => {
         hs_object_id: contactData.id
       }
     });
-    upsertResult = "created";
+    upsertResult = 'created';
   }
-    let result: CreateUpdateUpsertResult = {
-      recordDetails: upsertRecord,
-      updateResult: upsertResult
-    };
+  let result: CreateUpdateUpsertResult = {
+    recordDetails: upsertRecord,
+    updateResult: upsertResult
+  };
 
-    return result;
-}
+  return result;
+};
 
 // Update function 2 - Try to create the record, fall back to update if that fails
 // Slower and will result in DB errors, but explicit tracking of created
@@ -114,63 +111,63 @@ const verboseCreateOrUpdate = async (contactData: SimplePublicObject) => {
         hs_object_id: contactData.id
       }
     });
-    updateResult = "created";
+    updateResult = 'created';
   } catch (error) {
     console.log(error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError ){
-      if (error.code === "P2002") {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        const contactDataWithEmail = contactData as ContactsWithEmail; // Tell TS we always have an email address in this case
         // failed on unique property (i.e. email)
         // Update  existing record by email, just add HS record id to record
         prismaRecord = await prisma.contacts.update({
-          where:{
-            email: contactData.properties.email
+          where: {
+            email: contactDataWithEmail.properties.email
           },
-          data: { // add the hs ID but don't update anything else
+          data: {
+            // add the hs ID but don't update anything else
             hs_object_id: contactData.id
           }
         });
-        updateResult = "hsID_updated"
-      } else { // some other known error but not existing email
+        updateResult = 'hsID_updated';
+      } else {
+        // some other known error but not existing email
         prismaRecord = {
           id: -1,
           email: contactData.properties.email,
           first_name: contactData.properties.firstname,
           last_name: contactData.properties.lastname,
           hs_object_id: contactData.id
-        }
+        };
         updateResult = error.code; // log Prisma error code, will be tracked as error in results
       }
-    } else {  // Any other failed create result
+    } else {
+      // Any other failed create result
       prismaRecord = {
         id: -1,
         email: contactData.properties.email,
         first_name: contactData.properties.firstname,
         last_name: contactData.properties.lastname,
         hs_object_id: contactData.id
-      }
-      updateResult = "failed";
+      };
+      updateResult = 'failed';
     }
   }
 
   let result: CreateUpdateUpsertResult = {
     recordDetails: prismaRecord,
     updateResult: updateResult
-  }
+  };
   return result;
-}
+};
 
 // Initial sync FROM HubSpot contacts TO (local) database
 const initialContactsSync = async () => {
-  console.log("started sync");
+  console.log('started sync');
   const customerId = getCustomerId();
   const accessToken = await getAccessToken(customerId);
-  const hubspotClient = new HubSpotClient({
-    accessToken,
-    limiterOptions: DEFAULT_LIMITER_OPTIONS
-  });
 
   // Track created/updated/upserted/any errors
-  let jobRunResults : JobRunResults = {
+  let jobRunResults: JobRunResults = {
     upsert: {
       count: 0,
       records: []
@@ -194,41 +191,42 @@ const initialContactsSync = async () => {
   };
 
   // Get all contacts using client
-  const allContactsResponse: SimplePublicObject[] = await hubspotClient.crm.contacts.getAll(
-    pageLimit,
-    after,
-    propertiesToGet,
-    propertiesToGetWithHistory,
-    associationsToGet,
-    getArchived,
-  );
+  const allContactsResponse: SimplePublicObject[] =
+    await hubspotClient.crm.contacts.getAll(
+      pageLimit,
+      after,
+      propertiesToGet,
+      propertiesToGetWithHistory,
+      associationsToGet,
+      getArchived
+    );
 
   console.log(`Found ${allContactsResponse.length} contacts`);
 
   for (const element of allContactsResponse) {
     let createOrUpdateContactResult: CreateUpdateUpsertResult;
-    if (useVerboseCreateOrUpdate){
+    if (useVerboseCreateOrUpdate) {
       createOrUpdateContactResult = await verboseCreateOrUpdate(element);
     } else {
       createOrUpdateContactResult = await upsertContact(element);
     }
     // Add to overall results based on result of create/update result
     switch (createOrUpdateContactResult.updateResult) {
-      case "upsert":
+      case 'upsert':
         jobRunResults.upsert.count += 1;
         jobRunResults.upsert.records.push(createOrUpdateContactResult);
         break;
-      case "created":
+      case 'created':
         jobRunResults.created.count += 1;
         jobRunResults.created.records.push(createOrUpdateContactResult);
         break;
-      case "hsID_updated":
+      case 'hsID_updated':
         jobRunResults.hsID_updated.count += 1;
         jobRunResults.hsID_updated.records.push(createOrUpdateContactResult);
         break;
-      case "failed":
+      case 'failed':
         jobRunResults.failed.count += 1;
-        jobRunResults.failed.records.push(createOrUpdateContactResult)
+        jobRunResults.failed.records.push(createOrUpdateContactResult);
         break;
       default:
         jobRunResults.errors.count += 1;
@@ -237,10 +235,10 @@ const initialContactsSync = async () => {
     }
   }
 
-  return({
+  return {
     total: allContactsResponse.length,
-    results:jobRunResults
-  });
-}
+    results: jobRunResults
+  };
+};
 
-export {initialContactsSync};
+export { initialContactsSync };
